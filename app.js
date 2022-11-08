bb64d0cb13c0d67d7ad95054e54c327c561906c2
Cristina Rosace <rosacec@flip2.engr.oregonstate.edu>
2022-11-07 19:34:05 -0800

    installed packages for server, organized directory, rerouted pages, implemented handlebars, connected db and reading patients table

--- app.js
+++ app.js
@@ -8,42 +8,56 @@ var app = express();
 PORT = 9130;
 
 // Database
-var db = require('./database/db-connector')
+var db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.use(express.static('public'))
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 /*
     ROUTES
 */
 app.get('/', function(req, res)
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        let base = "<h1>MySQL Results:</h1>"
-                        res.send(base + JSON.stringify(results));
-                    });
-                });
-            });
-        });
+        res.render('homepage');
     });
 
+app.get('/patients', function(req, res)
+   {
+        let query1 = "SELECT * FROM Patients;";
+
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('./patient_pages/patients', {data:rows});
+        })
+   });
+
+app.get('/doctors', function(req, res)
+   {
+        res.render('./doctor_pages/doctors')
+   });
+
+app.get('/appointments', function(req, res)
+   {
+        res.render('./appt_pages/appointments')
+   });
+
+app.get('/medications', function(req, res)
+   {
+        res.render('./med_pages/medications')
+   });
+
+app.get('/prescriptions', function(req, res)
+   {
+        res.render('./script_pages/prescriptions')
+   });
+
+app.get('/appt_scripts', function(req, res)
+   {
+        res.render('appts_has_scripts')
+   });
 /*
     LISTENER
 */
