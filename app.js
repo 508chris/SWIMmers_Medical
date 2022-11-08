6e70c96be651c99e0ed21b66f1fb57c6fc057182
Chris <chris.l.digiacomo@gmail.com>
2022-11-08 10:21:01 -0500

    Added Create patient

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // Express
 var express = require('express');
 var app = express();
-PORT = 9130;
+PORT = 9001;
 
 // Database
 var db = require('./database/db-connector');
@@ -16,6 +16,10 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.use(express.static('public'))
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 
 /*
     ROUTES
@@ -25,6 +29,10 @@ app.get('/', function(req, res)
         res.render('homepage');
     });
 
+
+/* 
+    Patients Pages
+*/
 app.get('/patients', function(req, res)
    {
         let query1 = "SELECT * FROM Patients;";
@@ -33,6 +41,35 @@ app.get('/patients', function(req, res)
             res.render('./patient_pages/patients', {data:rows});
         })
    });
+
+app.get('/add_patient', function(req, res){
+    res.render('./patient_pages/add_patient')
+})
+
+
+app.post('/add-patient-form', function(req, res){
+    let data = req.body;
+    query1 = `INSERT INTO Patients (first_name, last_name, dob, address, email, phone_number, insurance) VALUES ('${data['input-patient-first-name']}', '${data['input-patient-last-name']}', '${data['input-dob']}', '${data['input-address']}', '${data['input-email']}', '${data['input-phone-number']}', '${data['input-insurance']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+        if (error) {
+            console.log(error);
+            res.sendStatus(400)
+        } else{
+            res.redirect('/patients')
+        }
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
 
 app.get('/doctors', function(req, res)
    {
