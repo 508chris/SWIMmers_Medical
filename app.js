f0a0d11ca5c6bd47a1b94c09eb6fce420079492d
Cristina Rosace <rosacec@flip3.engr.oregonstate.edu>
2022-11-08 07:49:51 -0800

    read and create doctor

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.use(express.static('public'))
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 /*
     ROUTES
@@ -35,9 +37,45 @@ app.get('/patients', function(req, res)
    });
 
 app.get('/doctors', function(req, res)
-   {
-        res.render('./doctor_pages/doctors')
-   });
+    {
+        let query1 = "SELECT * FROM Doctors;";
+
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('./doctor_pages/doctors', {data:rows});
+        })
+    });
+
+
+app.get('/add_doctor', function(req, res)
+    {
+         res.render('./doctor_pages/add_doctor')
+    });
+
+app.post('/add-doctor-form', function(req, res){
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+        console.log(data)
+        
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Doctors (first_name, last_name, specialty) VALUES ('${data['input-doctor-first-name']}', '${data['input-doctor-last-name']}', '${data['input-specialty']}')`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+    
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+            // presents it on the screen
+            else
+            {
+                res.redirect('/doctors');
+            }
+        })
+    })
 
 app.get('/appointments', function(req, res)
    {
